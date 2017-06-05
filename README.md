082878137bb31d4ac77fd311a6edf35e1de1a2af
Ryan Rempel <rgrempel@gmail.com>
2017-06-05 11:01:55 -0500

    Update documentation, and expose some lower-level helpers.

--- README.md
+++ README.md
@@ -2,14 +2,54 @@
 
 # elm-keyboard-event
 
-The existing Elm keyboard-related packages are focused on installing
-a global handler for keyboard events, to which one subscribes as
-needed.
+Most Elm keyboard-related packages (such as [elm-lang/keyboard][], and
+others which build on it) are focused on installing a global handler for
+keyboard events, to which one subscribes as needed.
 
-An alternative approach would be to listen for events in your `view`
-method, as one would ordinarily listen for HTML events in Elm.
-To do that, you would need to decode the `KeyboardEvent` which you
-will receive. This package provides such decoders.
+[elm-lang/keyboard]: http://package.elm-lang.org/packages/elm-lang/keyboard/latest
+
+An alternative approach would be to set up listeners for keyboard events in
+your `view` method, as one would ordinarily listen for HTML events in Elm.  To
+do that, you would need to decode the keyboard event which you will receive.
+This package provides such decoders, so you can do something like this:
+
+    div
+        [ on "keydown" <|
+            Json.Decode.map HandleKeyboardEvent decodeKeyboardEvent
+        , tabindex 0
+        , id "id-for-auto-focus"
+        , style [ ( "outline", "none" ) ]
+        ]
+        [ ... ]
+
+See the `examples` directory in the source code for complete examples.
+
+Compared to using subscriptions, one advantage of this approach is that you get
+more information from the keyboard event than the `KeyCode` which
+[elm-lang/keyboard][] supplies:
+
+  * the state of modifier keys (such as the shift key)
+
+  * whether the event is a "repeated" keyboard event (due to a key being held
+    down)
+
+Note that an HTML element must be focused in order to receive keyboard events
+(unlike in [elm-lang/keyboard][], since it attaches a listener to the
+Javascript `window` object). This is either an advantage or a disadvantage,
+depending on your circumstances. If you want to handle keyboard events
+differently depending on what is focused, it is an advantage. Otherwise, you
+can work around the need to focus, in this way:
+
+  * provide the element with a `tabindex` attribute (as demonstrated above),
+    so that it is focusable
+
+  * possibly give it a style of `outline: none;` to avoid the default outline
+    that would be drawn when the element is focused
+
+  * possibly use [elm-lang/dom][] to automatically focus the element when
+    you initialize the page
+
+[elm-lang/dom]: http://package.elm-lang.org/packages/elm-lang/dom/latest
 
 ## API
 
@@ -29,3 +69,5 @@ Try something like:
     cd elm-keyboard-event
     npm install
     npm test
+
+You can then find the compiled examples in the `examples` folder.
--- src/Keyboard/Event.elm
+++ src/Keyboard/Event.elm
@@ -1,17 +1,68 @@
-module Keyboard.Event exposing (KeyboardEvent, decodeKeyboardEvent, considerKeyboardEvent)
+module Keyboard.Event exposing (KeyboardEvent, decodeKeyboardEvent, considerKeyboardEvent, KeyCode, decodeKeyCode, decodeKey)
 
-{-| The existing Elm keyboard-related packages are focused on installing
-a global handler for keyboard events, to which one subscribes as
-needed.
+{-|
+Most Elm keyboard-related packages (such as [elm-lang/keyboard][], and
+others which build on it) are focused on installing a global handler for
+keyboard events, to which one subscribes as needed.
 
-An alternative approach would be to listen for events in your `view`
-method, as one would ordinarily listen for HTML events in Elm.
-To do that, you would need to decode the `KeyboardEvent` which you
-will receive. This package provides such decoders.
+[elm-lang/keyboard]: http://package.elm-lang.org/packages/elm-lang/keyboard/latest
 
-Also mention `tabindex` and `focus` and `outline`.
+An alternative approach would be to set up listeners for keyboard events in
+your `view` method, as one would ordinarily listen for HTML events in Elm.  To
+do that, you would need to decode the keyboard event which you will receive.
+This package provides such decoders, so you can do something like this:
+
+    div
+        [ on "keydown" <|
+            Json.Decode.map HandleKeyboardEvent decodeKeyboardEvent
+        , tabindex 0
+        , id "id-for-auto-focus"
+        , style [ ( "outline", "none" ) ]
+        ]
+        [ ... ]
+
+See the `examples` directory in the source code for complete examples.
+
+Compared to using subscriptions, one advantage of this approach is that you get
+more information from the keyboard event than the `KeyCode` which
+[elm-lang/keyboard][] supplies:
+
+  * the state of modifier keys (such as the shift key)
+
+  * whether the event is a "repeated" keyboard event (due to a key being held
+    down)
+
+Note that an HTML element must be focused in order to receive keyboard events
+(unlike in [elm-lang/keyboard][], since it attaches a listener to the
+Javascript `window` object). This is either an advantage or a disadvantage,
+depending on your circumstances. If you want to handle keyboard events
+differently depending on what is focused, it is an advantage. Otherwise, you
+can work around the need to focus, in this way:
+
+  * provide the element with a `tabindex` attribute (as demonstrated above),
+    so that it is focusable
+
+  * possibly give it a style of `outline: none;` to avoid the default outline
+    that would be drawn when the element is focused
+
+  * possibly use [elm-lang/dom][] to automatically focus the element when
+    you initialize the page
+
+[elm-lang/dom]: http://package.elm-lang.org/packages/elm-lang/dom/latest
+
+## KeyboardEvent
+
+Decode a `KeyboardEvent` from an HTML [keyboard event][].
+
+[keyboard event]: https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent
 
 @docs KeyboardEvent, decodeKeyboardEvent, considerKeyboardEvent
+
+## Helpers
+
+Some lower-level helpers that you might find useful.
+
+@docs KeyCode, decodeKeyCode, decodeKey
 
 -}
 
@@ -20,19 +71,25 @@ import Keyboard.Key exposing (Key, fromCode)
 import String
 
 
-{-| Decodes `keyCode` or `which` to get a numeric code for the key.
-+Note that the numeric code does not change when the shift key is
-+held down -- for that, you can decode the `.shfitKey`
+{-| A type alias for `Int`, as in [elm-lang/keyboard][].
 -}
-decodeKeyCode : Decoder Int
+type alias KeyCode =
+    Int
+
+
+{-| Decodes `keyCode`, `which` or `charCode` from a [keyboard event][] to get a
+numeric code for the key that was pressed.
+
+[keyboard event]: https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent
+-}
+decodeKeyCode : Decoder KeyCode
 decodeKeyCode =
     oneOf
         [ field "keyCode" decodeNonZero
         , field "which" decodeNonZero
         , field "charCode" decodeNonZero
-
-        -- In principle, we should always get some code, so instead
-        -- of making this a Maybe, we succeed with 0.
+          -- In principle, we should always get some code, so instead
+          -- of making this a Maybe, we succeed with 0.
         , succeed 0
         ]
 
@@ -51,7 +108,10 @@ decodeNonZero =
         int
 
 
-{-| Decodes the key, but only if it's not blank.
+{-| Decodes the `key` field from a [keyboard event][]. Results in
+`Nothing` if the `key` field is not present, or blank.
+
+[keyboard event]: https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent
 -}
 decodeKey : Decoder (Maybe String)
 decodeKey =
@@ -66,16 +126,19 @@ decodeKey =
         |> maybe
 
 
-{-| A keyboard event.
+{-| A representation of a [keyboard event][].
 
 The `key` field may or may not be present, depending on the listener ("keydown"
 vs. "keypress" vs. "keyup"), browser, and key pressed (character key vs.
-special key).
+special key). If not present, it will be `Nothing` here.
 
 The `keyCode` is normalized by `decodeKeyboardEvent` to use whichever of
 `which`, `keyCode` or `charCode` is provided, and made type-safe via
-`Keyboard.Key` (see the excellent `SwiftsNamesake/proper-keyboard` for
+`Keyboard.Key` (see the excellent [SwiftsNamesake/proper-keyboard][] for
 further manipulation of a `Key`).
+
+[keyboard event]: https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent
+[SwiftsNamesake/proper-keyboard]: http://package.elm-lang.org/packages/SwiftsNamesake/proper-keyboard/latest
 
 -}
 type alias KeyboardEvent =
@@ -89,7 +152,9 @@ type alias KeyboardEvent =
     }
 
 
-{-| Decodes a keyboard event.
+{-| Decodes a `KeyboardEvent` from a [keyboard event][].
+
+[keyboard event]: https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent
 -}
 decodeKeyboardEvent : Decoder KeyboardEvent
 decodeKeyboardEvent =
@@ -103,12 +168,17 @@ decodeKeyboardEvent =
         (field "shiftKey" bool)
 
 
-{-| You provide a function which, given a KeyboardEvent, turns it into a
-msg you can handle. You get back a `Decoder`.
+{-| You provide a function which, given a `KeyboardEvent`, turns it into a
+message your `update` function can handle. You get back a `Decoder` for those
+messages.
 
-If your function returns `Nothing`, then the decoder will fail (which is
-probably fine -- you're just ignoring that keyboard event).
+When your function returns `Nothing`, the decoder will fail. This means that
+the event will simply be ignored -- that is, it will not reach your `update`
+function at all.
 
+Essentially, this allows you to filter keyboard events inside the decoder
+itself, rather than in the `update` function. Whether this is a good idea or
+not will depend on your scenario.
 -}
 considerKeyboardEvent : (KeyboardEvent -> Maybe msg) -> Decoder msg
 considerKeyboardEvent func =
